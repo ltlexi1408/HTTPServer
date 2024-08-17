0ad161751ea9149584ab491a9cb50b34284e7de4
ltlexi140 <ltlexi1408@proton.me>
2024-08-17 20:58:35 +1000

    added graceful exit

--- httpserver.cpp
+++ httpserver.cpp
@@ -5,12 +5,26 @@
 #include <string>
 #include <cstring>
 #include <unistd.h>
+#include <csignal>
 
 #define PORT 8080
 
+int HTTPServer;
+
+void signalHandler(int signal){
+
+    if(HTTPServer >= 0){
+        close(HTTPServer);
+        std::cout << "Closing Server" << std::endl;
+    }
+
+    exit(signal);
+}
+
 int main(){
 
-    int HTTPServer;
+    std::signal(SIGINT, signalHandler);
+
     char html[1024] = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html><html><body><h1>C++ http server</h1><p>Testing.</p></body></html> ";
 
     if((HTTPServer = socket(AF_INET, SOCK_STREAM, 0 )) < 0){
@@ -33,20 +47,23 @@ int main(){
         return -1;
     }
 
-    socklen_t addressLength = sizeof(address);
-    int new_request = accept(HTTPServer, (struct sockaddr*)&address, &addressLength);
-    
-    if(new_request < 0){
-        std::cerr << strerror(errno) << std::endl;
-        return -1;
+    while(true){
+        socklen_t addressLength = sizeof(address);
+        int new_request = accept(HTTPServer, (struct sockaddr*)&address, &addressLength);
+        
+        if(new_request < 0){
+            std::cerr << strerror(errno) << std::endl;
+            return -1;
+        }
+
+        if(send(new_request, html, strlen(html), 0) < 0){
+            std::cerr << strerror(errno) << std::endl;
+            return -1;
+        }
+
+        close(new_request);
     }
 
-    if(send(new_request, html, strlen(html), 0) < 0){
-        std::cerr << strerror(errno) << std::endl;
-        return -1;
-    }
-
-    close(new_request);
     close(HTTPServer);
 
     return 0;
